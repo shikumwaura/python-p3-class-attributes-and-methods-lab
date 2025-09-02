4a0a7a0dd83bc92d74840742d63905948b5cc004
Wanjiku <wanjikumwaura0304@gmail.com>
2025-09-02 19:06:49 +0300

    complete lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,28 @@
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Increment total song count
+        Song.count += 1
+
+        # Update genre sets and counts
+        Song.genres.add(genre)
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # Update artist sets and counts
+        Song.artists.add(artist)
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
